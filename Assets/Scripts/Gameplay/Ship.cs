c9d7e1e44b4dd4b18b4e3218a84463d72f260578
RedTn <redmond.tran@gmail.com>
2017-08-17 20:30:33 -0700

    initial files

---
+++ Assets/Scripts/Gameplay/Ship.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Class for the ship object.
+public class Ship : MonoBehaviour {
+
+	#region PUBLIC VARIABLES
+	// The rotation speed of the ship, in degrees per second.
+	public float rotationSpeed = 180f;
+
+	// The movement speed of the ship, in force applied per second.
+	public float movementSpeed = 500f;
+
+	// A reference to the transform where bullets will be spawned from.
+	public Transform launcher;
+	#endregion
+
+	#region PRIVATE VARIABLES
+	private const string TURN_COROUTINE_NAME = "TurnTowardsPointAndShootCoroutine";
+
+	private GameManager gameManager;
+
+	private bool turning = false;
+	#endregion
+
+	#region MONOBEHAVIOUR METHODS
+	void Start()
+	{
+		gameManager = GameManager.Instance;
+	}
+	#endregion
+	
+	#region PUBLIC METHODS
+	public void OnHit()
+	{
+		gameManager.LoseLife();
+		StartCoroutine(StartInvincibilityTimer(2.5f));
+	}
+	#endregion
+
+	#region PRIVATE METHODS
+	// Shoot a bullet forward.
+	private void Shoot()
+	{
+		Bullet bullet = PoolManager.Instance.Spawn(Constants.BULLET_PREFAB_NAME).GetComponent<Bullet>();
+		bullet.SetPosition(launcher.position);
+		bullet.SetTrajectory(bullet.transform.position + transform.forward);
+	}
+
+	// Make the ship invincible for a time.
+	private IEnumerator StartInvincibilityTimer(float timeLimit)
+	{
+		collider2D.enabled = false;
+
+		SpriteRenderer spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+
+		float timer = 0;
+		float blinkSpeed = 0.25f;
+
+		while (timer < timeLimit)
+		{
+			yield return new WaitForSeconds(blinkSpeed);
+
+			spriteRenderer.enabled = !spriteRenderer.enabled;
+			timer += blinkSpeed;
+		}
+
+		spriteRenderer.enabled = true;
+		collider2D.enabled = true;
+	}
+	#endregion
+}
